26e6e20bc7eb6619bcf476e1c7edc9260e6d44bf
鈴木 健太郎 <szkkntru2@gmail.com>
2024-10-30 09:37:39 +0900

    test 'black'

---
+++ src/target.py
@@ -0,0 +1,34 @@
+# クォーテーションが、ダブルではなく、シングルになっている場合
+"""
+hoge
+"""
+
+# Listの要素間のスペースが不ぞろいかつ、改行されてしまっている
+list_1 = [1, 2, 3, 4, 5]
+
+# Listの要素のカンマが要素の前に置いているものと後に置いているものとが混在している
+list_2 = ["a", "b", "c", "d", "e"]
+
+
+# 正しく記載している関数⇒書き換わらないことを期待
+def func_1(vals: list) -> int:
+    return sum(vals)
+
+
+# 引数が小文字ではなく、大文字⇒小文字に書き換わってほしい
+def func_2(VALS: list) -> int:
+    return sum(VALS)
+
+
+# 関数名が小文字ではなく、大文字⇒全て小文字に書き換わってほしい
+def Func_3(vals: list) -> int:
+    return sum(vals)
+
+
+print(f"{list_1=}")
+print(f"{list_2=}")
+
+
+print(f"{func_1(list_1)=}")
+print(f"{func_2(list_1)=}")
+print(f"{Func_3(list_1)=}")
